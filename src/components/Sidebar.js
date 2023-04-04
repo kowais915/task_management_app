00c2b0ef662e0ca8567babe037f26735130511b3
Owais Khan <45107209+kowais915@users.noreply.github.com>
2023-04-04 09:24:18 +0500

    :art: improved the sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,28 @@
+import {useState, useEffect } from 'react';
+
+
+// importing styles
 import '../styles/Sidebar.css'
 
 const Sidebar = () => {
+    const [tabs, setTabs ] = useState([
+        {title: "Platform Launch", id: 1},
+        {title: "Product Launch", id: 2},
+        {title: "Marketing", id: 3},
+        {title: "Sales", id: 4},
+    ]);
+
     return ( 
         <div className="sidebar">
-            This is the sidebar
+            <h4>ALL   BOARDS</h4>
+
+            {tabs.map((tab) => {
+                return (
+                    <div className="tab" key={tab.id}>
+                        {tab.title}
+                    </div>
+                )
+            })}
         </div>
      );
 }
