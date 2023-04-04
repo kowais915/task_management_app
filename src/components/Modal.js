7c6a18811bb2fb27f060dffee2605a85db246251
Owais Khan <45107209+kowais915@users.noreply.github.com>
2023-04-05 00:59:15 +0500

    :recycle: added a modal component

---
+++ src/components/Modal.js
@@ -0,0 +1,16 @@
+
+
+
+const Modal = () => {
+    return ( 
+        <div className="backdrop">
+            <div className="modal">
+                <form>
+                    <h1>This is the modal</h1>
+                </form>
+            </div>
+        </div>
+     );
+}
+ 
+export default Modal;
